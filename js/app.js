73a7e4dff6f616f8a6d8ffe352542cf2751632ef
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-25 19:21:30 -0600

    finished

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function addAandBandMultiplyBy5 (a, b){
+    return (a + b) * 5;
+}
+
+console.log(addAandBandMultiplyBy5(5, 7));
+
+function returnTrueIfStringLengthIs10ElseFalse (word){
+    if(word.length === 10){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(returnTrueIfStringLengthIs10ElseFalse("word"));
+console.log(returnTrueIfStringLengthIs10ElseFalse("wordpressr"));
+console.log(returnTrueIfStringLengthIs10ElseFalse("word"));
